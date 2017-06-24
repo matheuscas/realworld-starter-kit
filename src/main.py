dbbb87b97d34b3a668368f020ee7d31515209a2b
Matheus Cardoso <matheus.mcas@gmail.com>
2017-06-24 19:09:26 -0300

    use of article blueprints

--- src/main.py
+++ src/main.py
@@ -1,22 +1,19 @@
 import asyncio
 import uvloop
 import db
+import article
 from sanic import Sanic
-from sanic.response import text, json
+from sanic.response import text
 
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
 app = Sanic(__name__)
 app.register_blueprint(db.bp_db)
+app.register_blueprint(article.bp_article)
 
 @app.route("/")
 async def test(request):
     return text('It is up and running')
-
-@app.route("/articles")
-async def get_articles(request):
-    articles = await app.config.db.articles.find().to_list(None)
-    return json(articles)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000, debug=True)
