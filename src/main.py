5307bea539fc99240cdb3e4c81bd275898ab7e00
Matheus Cardoso <matheus.mcas@gmail.com>
2017-06-23 22:42:34 -0300

    better initial main with mongo connection

--- src/main.py
+++ src/main.py
@@ -1,10 +1,24 @@
+import asyncio
+import uvloop
+import db
 from sanic import Sanic
-from sanic.response import text
+from sanic.response import text, json
+
+asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
 app = Sanic(__name__)
+app.register_blueprint(db.bp_db)
 
 @app.route("/")
 async def test(request):
-    return text('Hello RELOADED world!?')
+    return text('It is up and running')
 
-app.run(host="0.0.0.0", port=8000, debug=True)
+@app.route("/articles")
+async def get_articles(request):
+    articles = await app.config.db.articles.find().to_list(None)
+    return json(articles)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8000, debug=True)
+
+
