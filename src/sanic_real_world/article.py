805901ba1e5360f2731a76458fe2c356e0d36b87
Matheus Cardoso <matheus.mcas@gmail.com>
2017-06-25 00:34:19 -0300

    get article by slug

--- src/sanic_real_world/article.py
+++ src/sanic_real_world/article.py
@@ -3,8 +3,8 @@ from sanic import Blueprint
 
 bp_article = Blueprint('article')
 
-@bp_article.route("/articles")
-async def get_articles(request):
-    mongo_articles = await request.app.config['db'].articles.find().to_list(None)
-    articles = list(map(lambda a: {'article': a['article']}, mongo_articles))
-    return json(articles)
+@bp_article.route("/articles/<slug>")
+async def get_articles(request, slug):
+    mongo_articles = await request.app.config['db'].articles.find({'article.slug': slug}).to_list(None)
+    article = {'article': mongo_articles[0]['article']}
+    return json(article)
