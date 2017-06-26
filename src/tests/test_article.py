3c21c4ef0f77b8bc70871bc3a8976881ad6db598
Matheus Cardoso <matheus.mcas@gmail.com>
2017-06-25 23:29:41 -0300

    Testing to 'All Articles'

--- src/tests/test_article.py
+++ src/tests/test_article.py
@@ -8,32 +8,55 @@ def articles_slug_fixture():
     req, res = app.test_client.get('/articles/how-to-train-your-dragon')
     return {'req': req, 'res': res}
 
-@pytest.fixture
-def articles_fixture():
-    req, res = app.test_client.get('/articles')
-    return {'req': req, 'res': res}
+@pytest.fixture(params=['/articles/how-to-train-your-dragon', '/articles'])
+def articles_fixture(request):
+    req, res = app.test_client.get(request.param)
+    return {'req': req, 'res': res, 'result': json.loads(res.body)}
 
-def test_get_article_returns_200(articles_slug_fixture):
-    assert articles_slug_fixture['res'].status == 200
+def test_get_article_returns_200(articles_fixture):
+    'Response code is 200 OK'
 
-def test_response_contains_article(articles_slug_fixture):
-    assert 'article' in json.loads(articles_slug_fixture['res'].body)
+    assert articles_fixture['res'].status == 200
 
-def test_response_contains_title(articles_slug_fixture):
-    article = json.loads(articles_slug_fixture['res'].body)
-    assert 'title' in article['article']
+def test_response_contains_article(articles_fixture):
+    'Response contains "articles" property'
 
-def test_response_contains_slug(articles_slug_fixture):
-    article = json.loads(articles_slug_fixture['res'].body)
-    assert 'slug' in article['article']
+    if isinstance(articles_fixture['result'], list):
+        assert 'article' in articles_fixture['result'][0]
+    else:
+        assert 'article' in articles_fixture['result']
 
-def test_response_contains_body(articles_slug_fixture):
-    article = json.loads(articles_slug_fixture['res'].body)
-    assert 'body' in article['article']
+def test_response_contains_title(articles_fixture):
+    'Article has "title" property'
 
-def test_response_contains_createdAt(articles_slug_fixture):
-    article = json.loads(articles_slug_fixture['res'].body)
-    assert 'createdAt' in article['article']
+    if isinstance(articles_fixture['result'], list):
+        assert 'title' in articles_fixture['result'][0]['article']
+    else:
+        assert 'title' in articles_fixture['result']['article']
+
+def test_response_contains_slug(articles_fixture):
+    'Article has "slug" property'
+
+    if isinstance(articles_fixture['result'], list):
+        assert 'slug' in articles_fixture['result'][0]['article']
+    else:
+        assert 'slug' in articles_fixture['result']['article']
+
+def test_response_contains_body(articles_fixture):
+    'Article has "body" property'
+
+    if isinstance(articles_fixture['result'], list):
+        assert 'body' in articles_fixture['result'][0]['article']
+    else:
+        assert 'body' in articles_fixture['result']['article']
+
+def test_response_contains_createdAt(articles_fixture):
+    'Article has "createdAt" property'
+
+    if isinstance(articles_fixture['result'], list):
+        assert 'createdAt' in articles_fixture['result'][0]['article']
+    else:
+        assert 'createdAt' in articles_fixture['result']['article']
 
 # def test_response_createdAt_ISO8601_timestamp(articles_slug_fixture):
 #     article = json.loads(articles_slug_fixture['res'].body)
@@ -45,33 +68,59 @@ def test_response_contains_createdAt(articles_slug_fixture):
 #     date = arrow.get(article['article']['updatedAt'])
 #     assert date.isoformat() == article['article']['updatedAt']
 
-def test_response_contains_description(articles_slug_fixture):
-    article = json.loads(articles_slug_fixture['res'].body)
-    assert 'description' in article['article']
+def test_response_contains_description(articles_fixture):
+    'Article has "description" property'
 
-def test_response_contains_tagList(articles_slug_fixture):
-    article = json.loads(articles_slug_fixture['res'].body)
-    assert 'tagList' in article['article']
+    if isinstance(articles_fixture['result'], list):
+        assert 'description' in articles_fixture['result'][0]['article']
+    else:
+        assert 'description' in articles_fixture['result']['article']
 
-def test_response_tagList_is_list(articles_slug_fixture):
-    article = json.loads(articles_slug_fixture['res'].body)
-    assert type(article['article']['tagList']) == list
+def test_response_contains_tagList(articles_fixture):
+    'Article has "tagList" property'
 
-def test_response_contains_author(articles_slug_fixture):
-    article = json.loads(articles_slug_fixture['res'].body)
-    assert 'author' in article['article']
+    if isinstance(articles_fixture['result'], list):
+        assert 'tagList' in articles_fixture['result'][0]['article']
+    else:
+        assert 'tagList' in articles_fixture['result']['article']
 
-def test_response_contains_favorited(articles_slug_fixture):
-    article = json.loads(articles_slug_fixture['res'].body)
-    assert 'favorited' in article['article']
+def test_response_tagList_is_list(articles_fixture):
+    'Article\'s "tagList" property is an Array'
 
-def test_response_contains_favoritesCount(articles_slug_fixture):
-    article = json.loads(articles_slug_fixture['res'].body)
-    assert 'favoritesCount' in article['article']
+    if isinstance(articles_fixture['result'], list):
+        assert isinstance(articles_fixture['result'][0]['article']['tagList'], list)
+    else:
+        assert isinstance(articles_fixture['result']['article']['tagList'], list)
 
-def test_response_favoritesCount_is_integer(articles_slug_fixture):
-    article = json.loads(articles_slug_fixture['res'].body)
-    assert type(article['article']['favoritesCount']) == int
+def test_response_contains_author(articles_fixture):
+    'Article has "author" property'
 
-def test_get_articles_returns_200(articles_fixture):
-    assert articles_fixture['res'].status == 200
+    if isinstance(articles_fixture['result'], list):
+        assert 'author' in articles_fixture['result'][0]['article']
+    else:
+        assert 'author' in articles_fixture['result']['article']
+
+def test_response_contains_favorited(articles_fixture):
+    'Article has "favorited" property'
+
+    if isinstance(articles_fixture['result'], list):
+        assert 'favorited' in articles_fixture['result'][0]['article']
+    else:
+        assert 'favorited' in articles_fixture['result']['article']
+
+def test_response_contains_favoritesCount(articles_fixture):
+    'Article has "favoritesCount" property'
+
+    if isinstance(articles_fixture['result'], list):
+        assert 'favoritesCount' in articles_fixture['result'][0]['article']
+    else:
+        assert 'favoritesCount' in articles_fixture['result']['article']
+
+def test_response_favoritesCount_is_integer(articles_fixture):
+    'favoritesCount is an integer'
+
+    if isinstance(articles_fixture['result'], list):
+        assert isinstance(articles_fixture['result'][0]['article']['favoritesCount'], int)
+    else:
+        assert isinstance(articles_fixture['result']['article']['favoritesCount'], int)
+
