7a7a4e28d601cf4073a330fdc4e20e9967850ac9
Matheus Cardoso <matheus.mcas@gmail.com>
2017-06-26 11:02:26 -0300

    better population of articles

--- src/blueprints/db.py
+++ src/blueprints/db.py
@@ -9,28 +9,52 @@ async def register_db(app, loop):
     mongo_client = AsyncIOMotorClient('localhost', 27017)
     app.config.db = mongo_client['real_world']
     app.config.mongo = mongo_client
-    article = {
-        "article": {
-            "slug": "how-to-train-your-dragon",
-            "title": "How to train your dragon",
-            "description": "Ever wonder how?",
-            "body": "It takes a Jacobian",
-            "tagList": ["dragons", "training"],
-            "createdAt": "2016-02-18T03:22:56.637Z",
-            "updatedAt": "2016-02-18T03:48:35.824Z",
-            "favorited": False,
-            "favoritesCount": 0,
-            "author": {
-                "username": "jake",
-                "bio": "I work at statefarm",
-                "image": "https://i.stack.imgur.com/xHWG8.jpg",
-                "following": False
-            }
-        }
-    }
-    await app.config.db.articles.drop()
-    app.config.db.articles.insert_one(article)
+    await populateArticles(app)
 
 @bp_db.listener('after_server_stop')
 async def close_db(app, loop):
     app.config.mongo.close()
+
+async def populateArticles(app):
+    articles = [
+        {
+            "article": {
+                "slug": "how-to-train-your-dragon",
+                "title": "How to train your dragon",
+                "description": "Ever wonder how?",
+                "body": "It takes a Jacobian",
+                "tagList": ["dragons", "training"],
+                "createdAt": "2016-02-18T03:22:56.637Z",
+                "updatedAt": "2016-02-18T03:48:35.824Z",
+                "favorited": False,
+                "favoritesCount": 0,
+                "author": {
+                    "username": "jake",
+                    "bio": "I work at statefarm",
+                    "image": "https://i.stack.imgur.com/xHWG8.jpg",
+                    "following": False
+                }
+            }
+        },
+        {
+            "article": {
+                "slug": "how-to-train-your-dragon-2",
+                "title": "How to train your dragon 2",
+                "description": "So toothless",
+                "body": "It a dragon",
+                "tagList": ["dragons", "training"],
+                "createdAt": "2016-02-18T03:22:56.637Z",
+                "updatedAt": "2016-02-18T03:48:35.824Z",
+                "favorited": False,
+                "favoritesCount": 0,
+                "author": {
+                    "username": "jake",
+                    "bio": "I work at statefarm",
+                    "image": "https://i.stack.imgur.com/xHWG8.jpg",
+                    "following": False
+                }
+            }
+        }
+    ]
+    await app.config.db.articles.drop()
+    app.config.db.articles.insert(articles)
